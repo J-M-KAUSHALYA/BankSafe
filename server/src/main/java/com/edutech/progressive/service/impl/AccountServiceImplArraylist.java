e47f80cde5685a5e68a92c1a6a3dc4218141b2f2
J-M-KAUSHALYA <dummy@gmail.com>
2026-01-17 07:23:06 +0000

    Day1Task

--- server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImplArraylist.java
@@ -1,6 +1,26 @@
 package com.edutech.progressive.service.impl;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.edutech.progressive.entity.Accounts;
 
 public class AccountServiceImplArraylist {
+
+   public List<Accounts> getAllAccounts() throws SQLException {
+        return null;
+   }
+
+   public int addAccount(Accounts accounts) throws SQLException {
+        return -1;
+   }
+
+   public List<Accounts> getAllAccountsSortedByBalance() throws SQLException {
+        return null;
+   }
+
+   public void emptyArrayList() {
+    
+   }
 
 }
--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
@@ -1,5 +1,43 @@
 package com.edutech.progressive.service.impl;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.edutech.progressive.dao.CustomerDAO;
+import com.edutech.progressive.dao.CustomerDAOImpl;
+import com.edutech.progressive.entity.Customers;
+
 public class CustomerServiceImpl {
+
+    private CustomerDAO customerDAO;
+
+    public CustomerServiceImpl(CustomerDAOImpl customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
+    public List<Customers> getAllCustomers() throws SQLException{
+        return null;
+    }
+
+    public int addCustomer(Customers customers) throws SQLException{
+        return -1;
+    }
+
+    public List<Customers> getAllCustomersSortedByName() throws SQLException{
+        return null;
+    }
+
+    public void updateCustomer(Customers customers) throws SQLException{
+
+    }
+
+    public void deleteCustomer(int customerId) throws SQLException{
+
+    }
+
+    public Customers getCustomerById(int customerId) throws SQLException{
+        return null;
+    }
+
 
 }
