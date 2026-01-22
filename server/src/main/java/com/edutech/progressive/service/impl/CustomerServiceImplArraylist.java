8a0ed22ef57606b7292f9863db27fa746086c7c1
J-M-KAUSHALYA <dummy@gmail.com>
2026-01-22 04:02:58 +0000

    Day6Task

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
@@ -1,40 +1,37 @@
 package com.edutech.progressive.service.impl;
-
+ 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.edutech.progressive.entity.Customers;
-
-public class CustomerServiceImplArraylist {
-
-    private static List<Customers> customersList = new ArrayList<>();
-
-    public List<Customers> getAllCustomers() throws SQLException{
-            return customersList;
+import com.edutech.progressive.service.CustomerService;
+ 
+@Service
+public class CustomerServiceImplArraylist implements CustomerService{
+    private static List<Customers> customerList = new ArrayList<>();
+ 
+    @Override
+    public List<Customers> getAllCustomers() throws SQLException {
+        return customerList;
     }
-
-    public int addCustomer(Customers customers) throws SQLException{
-            customersList.add(customers);
-            return customersList.size();
+ 
+    @Override
+    public int addCustomer(Customers customers) throws SQLException {
+        customerList.add(customers);
+        return customerList.size();
     }
-
+ 
+    @Override
     public List<Customers> getAllCustomersSortedByName() throws SQLException {
-            List<Customers> sortedList = new ArrayList<>(customersList);
-            Collections.sort(sortedList);
-            return sortedList;
+        Collections.sort(customerList);
+        return customerList;
     }
-
+ 
     public void emptyArrayList(){
-        customersList = new ArrayList<>();
+        customerList.clear();
     }
-
-    public Customers  getCustomerById(int customerId) throws SQLException{
-        for (Customers c : customersList) {
-                if(c.getCustomerId() == customerId) return c;
-        }
-        return null;
-    }
-
 }
